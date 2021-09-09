f6017a764edd23c204a7ff9041718bc55d4272ff
Mati <matibat@gmail.com>
2021-09-09 15:38:09 -0300

    feat(cli_v2): Adds CLICommand class PE-204

---
+++ src/CLICommand/cliCommand.ts
@@ -0,0 +1,53 @@
+import { Command } from 'commander';
+
+export type CommandName = string;
+export type ParameterName = string;
+export interface Parameter {
+	aliases: ParameterName[];
+	description: string;
+	default?: string;
+}
+
+export interface CommandDescriptor {
+	name: CommandName;
+	parameters: Parameter[];
+	action(options: any): void;
+}
+
+export class CLICommand {
+	// A singleton instance of the commander's program object
+	private static program: Command;
+	private static _doneSettingCommands = false;
+
+	constructor(private readonly commandDescription: CommandDescriptor, program?: Command) {
+		if (!CLICommand.program) {
+			CLICommand.program = program || new Command();
+		}
+		this.setCommand();
+	}
+
+	private setCommand(): void {
+		if (CLICommand._doneSettingCommands) {
+			// eslint-disable-next-line no-console
+			console.log(
+				`Won't set a command after parameters are parsed! \n${JSON.stringify(this.commandDescription, null, 4)}`
+			);
+			return;
+		}
+		this.setCommanderCommand();
+	}
+
+	private setCommanderCommand(): void {
+		const command = CLICommand.program.command(this.commandDescription.name);
+		this.commandDescription.parameters.forEach((parameter) => {
+			const aliasesAsString = parameter.aliases.join(' ');
+			command.option(aliasesAsString, parameter.description, parameter.default);
+		});
+		command.action(this.commandDescription.action);
+	}
+
+	public static doneSettingCommands(): void {
+		CLICommand.program.parse(process.argv);
+		CLICommand._doneSettingCommands = true;
+	}
+}
