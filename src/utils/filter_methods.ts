ce73b29a6bc9609463c784a0bad368d174c666f5
Mati <matibat@gmail.com>
2021-10-05 13:44:48 -0300

    feat(filter methods): removes unused method PE-322

--- src/utils/filter_methods.ts
+++ src/utils/filter_methods.ts
@@ -1,5 +1,4 @@
 import { ArFSFileOrFolderEntity } from '../arfsdao';
-import { FolderID } from '../types';
 
 /**
  * @name lastRevisionFilter is a standard JS find/filter function intended to
@@ -19,15 +18,3 @@ export function latestRevisionFilter(
 	const latestRevision = allRevisions[0];
 	return entity.txId === latestRevision.txId;
 }
-
-/**
- * @name childrenAndFolderOfFilterFactory is a factory function for a standard find/filter function
- * which filters all children and self of a specific folder
- *
- * @param {FolderID[]} folderIDs an array of the parent folder IDs to query for
- * @returns the find/filter function
- */
-export const childrenAndFolderOfFilterFactory = (folderIDs: FolderID[]) =>
-	function (entity: ArFSFileOrFolderEntity): boolean {
-		return folderIDs.includes(entity.parentFolderId) || folderIDs.includes(entity.entityId);
-	};
