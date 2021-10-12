e170af32812d4b062928da02f29510f5858457f1
Ariel Melendez <ariel@ardrive.io>
2021-10-11 19:42:21 -0700

    refactor(arfsdao): Remove unnecessary generic drive function PR-542

--- src/arfsdao.ts
+++ src/arfsdao.ts
@@ -621,33 +621,9 @@ export class ArFSDAO extends ArFSDAOAnonymous {
 		return transaction;
 	}
 
-	// Generic function for generic privacy use cases
-	async getDrive<
-		R extends ArFSPublicDrive | ArFSPrivateDrive,
-		B extends ArFSMetadataEntityBuilder<R>,
-		P extends ArFSMetadataEntityBuilderParams,
-		F extends ArFSMetadataEntityBuilderFactoryFunction<R, B, P>
-	>(builderFactory: F, builderFactoryParams: P): Promise<R> {
-		const driveBuilder = builderFactory(builderFactoryParams);
-		return await driveBuilder.build();
-	}
-
 	// Convenience function for known-private use cases
 	async getPrivateDrive(driveId: DriveID, driveKey: DriveKey): Promise<ArFSPrivateDrive> {
-		// Exemplifies use of the generic case although utilizing an ArFSPrivateDriveBuilder on its own would suffice
-		const driveBuilderFn = ({ entityId, arweave, key }: ArFSPrivateMetadataEntityBuilderParams) => {
-			return new ArFSPrivateDriveBuilder({ entityId, arweave, key });
-		};
-		return this.getDrive<
-			ArFSPrivateDrive,
-			ArFSPrivateDriveBuilder,
-			ArFSPrivateMetadataEntityBuilderParams,
-			ArFSMetadataEntityBuilderFactoryFunction<
-				ArFSPrivateDrive,
-				ArFSPrivateDriveBuilder,
-				ArFSPrivateMetadataEntityBuilderParams
-			>
-		>(driveBuilderFn, { entityId: driveId, arweave: this.arweave, key: driveKey });
+		return new ArFSPrivateDriveBuilder({ entityId: driveId, arweave: this.arweave, key: driveKey }).build();
 	}
 
 	// Generic function for generic privacy use cases
