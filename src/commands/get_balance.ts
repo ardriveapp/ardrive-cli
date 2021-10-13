609dcaa5037a3d25cf18abd67cf14921f46bfba5
Mati <matibat@gmail.com>
2021-10-12 23:59:36 -0300

    feat(get-balance command): Quote the balance in AR instead of Winston PE-550

--- src/commands/get_balance.ts
+++ src/commands/get_balance.ts
@@ -2,14 +2,19 @@ import { cliWalletDao } from '..';
 import { CLICommand, ParametersHelper } from '../CLICommand';
 import { AddressParameter, SeedPhraseParameter, WalletFileParameter } from '../parameter_declarations';
 
+const anArInWinston = 1_000_000_000_000;
+const numberOfDigitsAfterComma = 8;
+
 new CLICommand({
 	name: 'get-balance',
 	parameters: [WalletFileParameter, SeedPhraseParameter, AddressParameter],
 	async action(options) {
 		const parameters = new ParametersHelper(options);
 		const address = await parameters.getWalletAddress();
-		const balance = await cliWalletDao.getAddressWinstonBalance(address);
-		console.log(balance);
+		const balanceInWinston = await cliWalletDao.getAddressWinstonBalance(address);
+		const balanceInAR = balanceInWinston / anArInWinston;
+		const roundedBalanceInAR = balanceInAR.toFixed(numberOfDigitsAfterComma);
+		console.log(`${roundedBalanceInAR} AR`);
 		process.exit(0);
 	}
 });
