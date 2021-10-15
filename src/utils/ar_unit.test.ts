72009a65b5bfbd1f63639c9d4b5ad1a18a247add
Mati <matibat@gmail.com>
2021-10-15 19:28:51 -0300

    tests(assert digits precision): Adds tests for the assertARPrecision method PE-567

---
+++ src/utils/ar_unit.test.ts
@@ -0,0 +1,35 @@
+import { expect } from 'chai';
+import { assertARPrecision } from './ar_unit';
+
+describe('The assertARPrecision method', () => {
+	const VALID_INTEGER = '1';
+	const VALID_FLOATING_POINT = '0.1';
+	const VALID_FLOATING_POINT_WITH_TRAILING_ZEROS = '.00000000000100000000000000000';
+	const A_HALF_A_WINSTON = '.0000000000005';
+	const NOT_A_NUMBER = 'not a number >:b .00000';
+	const NEGATIVE_VALUE = '-10';
+
+	it('Passes when asserting an integer', () => {
+		expect(assertARPrecision.bind(this, VALID_INTEGER)).to.not.throw();
+	});
+
+	it('Passes when asserting a floating point', () => {
+		expect(assertARPrecision.bind(this, VALID_FLOATING_POINT)).to.not.throw();
+	});
+
+	it('Passes when asserting a valid floating point with trailing zeros at the end', () => {
+		expect(assertARPrecision.bind(this, VALID_FLOATING_POINT_WITH_TRAILING_ZEROS)).to.not.throw();
+	});
+
+	it('Throws when asserting a fraction of a Winston', () => {
+		expect(assertARPrecision.bind(this, A_HALF_A_WINSTON)).to.throw();
+	});
+
+	it('Throws when asserting an NaN', () => {
+		expect(assertARPrecision.bind(this, NOT_A_NUMBER)).to.throw();
+	});
+
+	it('Throws when asserting a negative value', () => {
+		expect(assertARPrecision.bind(this, NEGATIVE_VALUE)).to.throw();
+	});
+});
