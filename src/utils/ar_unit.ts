ddcfbbe8bb5e13e95d54e3d0fc30af426feb7ce0
Ariel Melendez <ariel@ardrive.io>
2021-10-16 16:51:07 -0700

    feat(send ar): Simplify regex for ar strings PE-567

--- src/utils/ar_unit.ts
+++ src/utils/ar_unit.ts
@@ -1,19 +1,21 @@
-export const MAX_DIGITS_OF_PRECISION = 12;
 /**
  * Capturing group 1: the points after the comma with trimmed zeros
  */
-export const FLOATING_POINT_REGEXP = /^\d*(?:\.(\d*?[0-9])0*)?$/;
+export const FLOATING_POINT_REGEXP = /^\d*\.\d{12}([1-9]+)$/;
 
 export function assertARPrecision(arAmount: string): void {
-	const floatingPointMatch = arAmount.match(FLOATING_POINT_REGEXP);
-	if (!floatingPointMatch) {
+	if (Number.isNaN(+arAmount)) {
 		throw new Error(`The AR amount must be a number. Got: ${arAmount}`);
 	}
-	const digitsOfPrecission = floatingPointMatch[1] || '';
-	const numberOfDigitsOfPrecision = digitsOfPrecission.length;
-	if (numberOfDigitsOfPrecision > MAX_DIGITS_OF_PRECISION) {
+
+	if (+arAmount < 0.0) {
+		throw new Error(`The AR amount must be a positive number. Got: ${arAmount}`);
+	}
+
+	const excessiveDigits = arAmount.match(FLOATING_POINT_REGEXP)?.[1] || '';
+	if (excessiveDigits.length) {
 		throw new Error(
-			`The AR amount must have a maximum of 12 digits of precision, but got ${numberOfDigitsOfPrecision}`
+			`The AR amount must have a maximum of 12 digits of precision, but got ${12 + excessiveDigits.length}`
 		);
 	}
 }
