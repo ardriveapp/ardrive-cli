c5ddeb10777e3f00cc29a26936d5e0ceedf0e307
Ariel Melendez <ariel@ardrive.io>
2021-10-19 15:32:37 -0700

    refactor(arweave address): Improve error message PE-582

--- src/arweave_address.ts
+++ src/arweave_address.ts
@@ -2,7 +2,7 @@ export class ArweaveAddress {
 	constructor(private readonly address: string) {
 		if (!address.match(new RegExp('^[a-zA-Z0-9_-]{43}$'))) {
 			throw new Error(
-				'Arweave addresses may only contain characters in the following character set: [a-zA-Z0-9_-]'
+				'Arweave addresses must be 43 characters in length with characters in the following set: [a-zA-Z0-9_-]'
 			);
 		}
 	}
