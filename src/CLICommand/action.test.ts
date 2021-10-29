a7286f83b9694a859ecd27799fb77d5a1ddbc3c4
Mati <matibat@gmail.com>
2021-10-29 10:20:53 -0300

    test:(cli action): adds some pending test cases PE-624

---
+++ src/CLICommand/action.test.ts
@@ -0,0 +1,9 @@
+describe('The CLIAction class', () => {
+	it('You can run one or more awaiters in some action');
+	it('Trigger resolves into a number');
+	it('The awaiter resolves into the parsedParameters');
+	it('There will be a static runningAction after some is triggered for instantly returning');
+	it('The awaiter rejects on trigger reject');
+	it('The awaiter rejects when a parsing error is reported');
+	it('The awaiter rejects when the action was not triggered');
+});
