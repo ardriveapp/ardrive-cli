d983dab8167b0b2f9cccf6a14a8fbc3e6367cf76
Ariel Melendez <ariel@ardrive.io>
2021-11-01 03:46:17 -0700

    test(entity id): Add unit tests for EntityID and EID PE-635

---
+++ src/types/entity_id.test.ts
@@ -0,0 +1,67 @@
+import { expect } from 'chai';
+import { EID, EntityID } from './entity_id';
+
+describe('EntityID class', () => {
+	describe('constructor', () => {
+		it('constructs valid EntityIDs given healthy inputs', () => {
+			const eidInputs = [
+				'00000000-0000-0000-0000-000000000000',
+				'99999999-9999-9999-9999-999999999999',
+				'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa',
+				'ffffffff-ffff-ffff-ffff-ffffffffffff'
+			];
+			eidInputs.forEach((eid) => expect(() => new EntityID(eid)).to.not.throw(Error));
+		});
+
+		it('throws an error when provided invalid inputs', () => {
+			const eidInputs = [
+				'',
+				'9999999909999-9999-9999-999999999999',
+				'99999999-999909999-9999-999999999999',
+				'99999999-9999-999909999-999999999999',
+				'99999999-9999-9999-99990999999999999',
+				'gggggggg-gggg-gggg-gggg-gggggggggggg',
+				'AAAAAAAA-AAAA-AAAA-AAAA-AAAAAAAAAAAA',
+				'FFFFFFFF-FFFF-FFFF-FFFF-FFFFFFFFFFFF',
+				'!@#$%^&*-()_+-=;"<->,./-?-----------'
+			];
+			eidInputs.forEach((eid) => expect(() => new EntityID(eid)).to.throw(Error));
+		});
+	});
+
+	describe('toString function', () => {
+		it('returns the correct Entity ID string', () => {
+			const eid = new EntityID('01234567-89ab-cdef-0000-000000000000');
+			expect(eid.toString()).to.equal('01234567-89ab-cdef-0000-000000000000');
+			expect(`${eid}`).to.equal('01234567-89ab-cdef-0000-000000000000');
+		});
+	});
+
+	describe('valueOf function', () => {
+		it('returns the correct Entity ID string value', () => {
+			const eid = new EntityID('01234567-89ab-cdef-0000-000000000000');
+			expect(eid.valueOf()).to.equal('01234567-89ab-cdef-0000-000000000000');
+		});
+	});
+
+	describe('isEqualTo function', () => {
+		it('correctly evaluates equality', () => {
+			const eid1 = new EntityID('01234567-89ab-cdef-0000-000000000000');
+			const eid2 = new EntityID('01234567-89ab-cdef-0000-000000000000');
+			const eid3 = new EntityID('01234567-89ab-cdef-0000-000000000001');
+			expect(eid1.isEqualTo(eid2), `${eid1} and ${eid2}`).to.be.true;
+			expect(eid2.isEqualTo(eid1), `${eid2} and ${eid1}`).to.be.true;
+			expect(eid1.isEqualTo(eid3), `${eid1} and ${eid3}`).to.be.false;
+			expect(eid3.isEqualTo(eid1), `${eid3} and ${eid1}`).to.be.false;
+			expect(eid2.isEqualTo(eid3), `${eid2} and ${eid3}`).to.be.false;
+			expect(eid3.isEqualTo(eid2), `${eid3} and ${eid2}`).to.be.false;
+		});
+	});
+});
+
+describe('EID function', () => {
+	it('returns the correct EntityID', () => {
+		const expected = new EntityID('01234567-89ab-cdef-0000-000000000000');
+		expect(`${EID('01234567-89ab-cdef-0000-000000000000')}`).to.equal(`${expected}`);
+	});
+});
