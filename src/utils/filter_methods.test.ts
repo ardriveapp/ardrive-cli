a4a7dde16493c47b10ff1d185991554a533cf49f
Ariel Melendez <ariel@ardrive.io>
2021-11-09 23:40:39 -0800

    test(filter methods): Add coverage for fileFilter and folderFilter functions PE-635

--- src/utils/filter_methods.test.ts
+++ src/utils/filter_methods.test.ts
@@ -1,8 +1,8 @@
 import { expect } from 'chai';
 import { ArFSPublicDrive } from '../arfs_entities';
 import { Mutable, TxID } from '../types';
-import { latestRevisionFilter, latestRevisionFilterForDrives } from './filter_methods';
-import { stubPublicDrive, stubPublicFile } from './stubs';
+import { fileFilter, folderFilter, latestRevisionFilter, latestRevisionFilterForDrives } from './filter_methods';
+import { stubPrivateFile, stubPrivateFolder, stubPublicDrive, stubPublicFile, stubPublicFolder } from './stubs';
 
 describe('The latestRevisionFilter function', () => {
 	it('returns true when only entry in array matches the search entry', () => {
@@ -41,5 +41,41 @@ describe('The latestRevisionFilterForDrives function', () => {
 		const stubDrive2 = stubPublicDrive();
 		(stubDrive2 as Mutable<ArFSPublicDrive>).txId = TxID('0000000000000000000000000000000000000000001');
 		expect(latestRevisionFilterForDrives(stubDrive, 0, [stubDrive2, stubDrive])).to.be.false;
+	});
+});
+
+describe('The fileFilter function', () => {
+	it('returns true for an ArFSPublicFile', () => {
+		expect(fileFilter(stubPublicFile({}))).to.be.true;
+	});
+
+	it('returns true for an ArFSPrivateFile', () => {
+		expect(fileFilter(stubPrivateFile({}))).to.be.true;
+	});
+
+	it('returns false for an ArFSPublicFolder', () => {
+		expect(fileFilter(stubPublicFolder({}))).to.be.false;
+	});
+
+	it('returns false for an ArFSPrivateFolder', () => {
+		expect(fileFilter(stubPrivateFolder({}))).to.be.false;
+	});
+});
+
+describe('The folderFilter function', () => {
+	it('returns false for an ArFSPublicFile', () => {
+		expect(folderFilter(stubPublicFile({}))).to.be.false;
+	});
+
+	it('returns false for an ArFSPrivateFile', () => {
+		expect(folderFilter(stubPrivateFile({}))).to.be.false;
+	});
+
+	it('returns false for an ArFSPublicFolder', () => {
+		expect(folderFilter(stubPublicFolder({}))).to.be.true;
+	});
+
+	it('returns false for an ArFSPrivateFolder', () => {
+		expect(folderFilter(stubPrivateFolder({}))).to.be.true;
 	});
 });
