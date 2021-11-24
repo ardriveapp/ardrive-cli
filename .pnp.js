643801616edd0754de9ace899965e3536af7fead
Derek Sonnenberg <derek@ardrive.io>
2021-11-24 14:26:08 -0600

    chore(version): Use alpha core release PE-477

--- .pnp.js
+++ .pnp.js
@@ -48,7 +48,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@types/source-map-support", "npm:0.5.4"],
             ["@typescript-eslint/eslint-plugin", "virtual:6f50bb9424c73c7612c66dab5cf8914d8ec79550c84d8ca5e4888e80022682c708b4b5a1c510d282a03285cc9bb19002b477ae70d15882aa995ea1d5d6bf24ab#npm:4.20.0"],
             ["@typescript-eslint/parser", "virtual:6f50bb9424c73c7612c66dab5cf8914d8ec79550c84d8ca5e4888e80022682c708b4b5a1c510d282a03285cc9bb19002b477ae70d15882aa995ea1d5d6bf24ab#npm:4.20.0"],
-            ["ardrive-core-js", "npm:1.0.3"],
+            ["ardrive-core-js", "npm:1.0.3-alpha-1"],
             ["arweave", "npm:1.10.16"],
             ["chai", "npm:4.3.4"],
             ["commander", "npm:8.3.0"],
@@ -1292,7 +1292,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@types/source-map-support", "npm:0.5.4"],
             ["@typescript-eslint/eslint-plugin", "virtual:6f50bb9424c73c7612c66dab5cf8914d8ec79550c84d8ca5e4888e80022682c708b4b5a1c510d282a03285cc9bb19002b477ae70d15882aa995ea1d5d6bf24ab#npm:4.20.0"],
             ["@typescript-eslint/parser", "virtual:6f50bb9424c73c7612c66dab5cf8914d8ec79550c84d8ca5e4888e80022682c708b4b5a1c510d282a03285cc9bb19002b477ae70d15882aa995ea1d5d6bf24ab#npm:4.20.0"],
-            ["ardrive-core-js", "npm:1.0.3"],
+            ["ardrive-core-js", "npm:1.0.3-alpha-1"],
             ["arweave", "npm:1.10.16"],
             ["chai", "npm:4.3.4"],
             ["commander", "npm:8.3.0"],
@@ -1317,10 +1317,10 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
         }]
       ]],
       ["ardrive-core-js", [
-        ["npm:1.0.3", {
-          "packageLocation": "./.yarn/cache/ardrive-core-js-npm-1.0.3-3eb958d43d-ee93abf5a7.zip/node_modules/ardrive-core-js/",
+        ["npm:1.0.3-alpha-1", {
+          "packageLocation": "./.yarn/cache/ardrive-core-js-npm-1.0.3-alpha-1-ff9281f640-d9b8302cb4.zip/node_modules/ardrive-core-js/",
           "packageDependencies": [
-            ["ardrive-core-js", "npm:1.0.3"],
+            ["ardrive-core-js", "npm:1.0.3-alpha-1"],
             ["arweave", "npm:1.10.16"],
             ["arweave-bundles", "npm:1.0.3"],
             ["arweave-mnemonic-keys", "npm:0.0.9"],
