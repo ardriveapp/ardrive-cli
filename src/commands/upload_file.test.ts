11b220b51242d0c4acc65075d25a5d97a8b9476c
Karl Prieb <karl@prieb.me>
2021-12-06 13:02:19 -0300

    test(glob-upload): simplify formatResults test PE-739
    Co-authored-by: arielmelendez <ariel.l.melendez@gmail.com>

--- src/commands/upload_file.test.ts
+++ src/commands/upload_file.test.ts
@@ -1,55 +1,90 @@
-import { randomUUID } from 'crypto';
 import { expect } from 'chai';
-import { ArFSResult, ArweaveAddress, EntityID, TransactionID, Winston } from 'ardrive-core-js';
+import { ADDR, ArFSResult, EID, TxID, W } from 'ardrive-core-js';
 
 import { formatResults } from './upload_file';
 
-// Any TxIDs, EntityIDS and Arweave addresses used on this mock are invalid as it's not those values that are being tested
-// Real values are not UUIDs.
-const createResultMock = (): ArFSResult => ({
-	created: [
-		{
-			type: 'file',
-			metadataTxId: (randomUUID() as unknown) as TransactionID,
-			dataTxId: (randomUUID() as unknown) as TransactionID,
-			entityId: (randomUUID() as unknown) as EntityID
-		}
-	],
-	tips: [
-		{
-			txId: (randomUUID() as unknown) as TransactionID,
-			recipient: (randomUUID() as unknown) as ArweaveAddress,
-			winston: new Winston(10000000)
-		}
-	],
-	fees: {
-		[randomUUID()]: new Winston(40121526),
-		[randomUUID()]: new Winston(40121526),
-		[randomUUID()]: new Winston(401226)
-	}
-});
-
 describe('Commands - upload-file', () => {
 	it('formatResults should merge multiple results in just one object', () => {
-		const firstResult = createResultMock();
-		const secondResult = createResultMock();
+		const firstResult: ArFSResult = {
+			created: [
+				{
+					type: 'file',
+					metadataTxId: TxID('0000000000000000000000000000000000000000000'),
+					dataTxId: TxID('0000000000000000000000000000000000000000001'),
+					entityId: EID('00000000-0000-0000-0000-000000000000'),
+					key: 'fakeKey1'
+				}
+			],
+			tips: [
+				{
+					txId: TxID('0000000000000000000000000000000000000000001'),
+					recipient: ADDR('abcdefghijklmnopqrxtuvwxyz123456789ABCDEFGH'),
+					winston: W(12345)
+				}
+			],
+			fees: {
+				'0000000000000000000000000000000000000000001': W(24680)
+			}
+		};
+
+		const secondResult: ArFSResult = {
+			created: [
+				{
+					type: 'file',
+					metadataTxId: TxID('0000000000000000000000000000000000000000002'),
+					dataTxId: TxID('0000000000000000000000000000000000000000003'),
+					entityId: EID('00000000-0000-0000-0000-000000000001'),
+					key: 'fakeKey2'
+				}
+			],
+			tips: [
+				{
+					txId: TxID('0000000000000000000000000000000000000000003'),
+					recipient: ADDR('123456789ABCDEFGHabcdefghijklmnopqrxtuvwxyz'),
+					winston: W(54321)
+				}
+			],
+			fees: {
+				'0000000000000000000000000000000000000000003': W(13579)
+			}
+		};
 
 		const resultsOutput = formatResults([firstResult, secondResult]);
 
 		// created
-		expect(resultsOutput.created[0]).to.deep.equal(firstResult.created[0]);
-		expect(resultsOutput.created[1]).to.deep.equal(secondResult.created[0]);
-
-		// tips
-		expect(resultsOutput.tips[0]).to.deep.equal(firstResult.tips[0]);
-		expect(resultsOutput.tips[1]).to.deep.equal(secondResult.tips[0]);
-
-		const firstFeeFromFirstResult = Object.keys(firstResult.fees)[0];
-		const firstFeeFromSecondResult = Object.keys(firstResult.fees)[0];
-		const resultsOutputFeeKeys = Object.keys(resultsOutput.fees);
-
-		// fees
-		expect(resultsOutputFeeKeys).to.include(firstFeeFromFirstResult);
-		expect(resultsOutputFeeKeys).to.include(firstFeeFromSecondResult);
+		expect(resultsOutput).to.deep.equal({
+			created: [
+				{
+					type: 'file',
+					metadataTxId: TxID('0000000000000000000000000000000000000000000'),
+					dataTxId: TxID('0000000000000000000000000000000000000000001'),
+					entityId: EID('00000000-0000-0000-0000-000000000000'),
+					key: 'fakeKey1'
+				},
+				{
+					type: 'file',
+					metadataTxId: TxID('0000000000000000000000000000000000000000002'),
+					dataTxId: TxID('0000000000000000000000000000000000000000003'),
+					entityId: EID('00000000-0000-0000-0000-000000000001'),
+					key: 'fakeKey2'
+				}
+			],
+			tips: [
+				{
+					txId: TxID('0000000000000000000000000000000000000000001'),
+					recipient: ADDR('abcdefghijklmnopqrxtuvwxyz123456789ABCDEFGH'),
+					winston: W(12345)
+				},
+				{
+					txId: TxID('0000000000000000000000000000000000000000003'),
+					recipient: ADDR('123456789ABCDEFGHabcdefghijklmnopqrxtuvwxyz'),
+					winston: W(54321)
+				}
+			],
+			fees: {
+				'0000000000000000000000000000000000000000001': W(24680),
+				'0000000000000000000000000000000000000000003': W(13579)
+			}
+		});
 	});
 });
