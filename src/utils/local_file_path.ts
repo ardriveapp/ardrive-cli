cea4bd441605dab7ce6d21f8805c27efeef75cd4
Mati <matibat@gmail.com>
2021-12-08 15:23:13 -0300

    doc(download file): adds a docblock PE-464

--- src/utils/local_file_path.ts
+++ src/utils/local_file_path.ts
@@ -3,15 +3,23 @@ import { basename, dirname, resolve } from 'path';
 
 export type FilePathAndName = [string, string?];
 
+/**
+ * Resolves the path, verifies its existance and type to conditionally return its dir and file name
+ * @param {string} destOutputPath - the path from where to extract the dir path and name
+ * @returns {FilePathAndName} - the directory where to put the file and the file name (which is undefined when the provided destOutputPath is a directory)
+ */
 export function getOutputFilePathAndName(destOutputPath: string): FilePathAndName {
 	const resolvedOutputPath = resolve(destOutputPath);
 	const outputDirname = dirname(resolvedOutputPath);
 	const outputBasename = basename(resolvedOutputPath);
 	try {
 		const outputPathStats = statSync(resolvedOutputPath);
+		// the destination does exist
 		if (outputPathStats.isDirectory()) {
+			// and is a directory
 			return [resolvedOutputPath];
 		} else if (outputPathStats.isFile()) {
+			// as an existing file
 			return [outputDirname, outputBasename];
 		}
 		throw new Error(`The destination isn't a folder nor a file!`);
