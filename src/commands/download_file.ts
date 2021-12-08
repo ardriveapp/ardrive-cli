7312b0f87e16205020b6393a54437c6295576ee0
Mati <matibat@gmail.com>
2021-12-08 15:03:45 -0300

    chore(download file): replaces positional arguments for a destructuring PE-464

--- src/commands/download_file.ts
+++ src/commands/download_file.ts
@@ -13,7 +13,7 @@ new CLICommand({
 		const fileId = parameters.getRequiredParameterValue(FileIdParameter, EID);
 		const destOutputPath = parameters.getParameterValue(DestinationOutputPathParameter) || './';
 
-		const [destFolderPath, fileName] = getOutputFilePathAndName(destOutputPath);
+		const [destFolderPath, defaultFileName] = getOutputFilePathAndName(destOutputPath);
 
 		if (await parameters.getIsPrivate()) {
 			const wallet = await parameters.getRequiredWallet();
@@ -23,10 +23,10 @@ new CLICommand({
 			});
 			const driveId = await ardrive.getDriveIdForFileId(fileId);
 			const driveKey = await parameters.getDriveKey({ driveId: driveId });
-			await ardrive.downloadPrivateFile(fileId, driveKey, destFolderPath, fileName);
+			await ardrive.downloadPrivateFile({ fileId, driveKey, destFolderPath, defaultFileName });
 		} else {
 			const ardrive = cliArDriveAnonymousFactory({});
-			await ardrive.downloadPublicFile(fileId, destFolderPath, fileName);
+			await ardrive.downloadPublicFile({ fileId, destFolderPath, defaultFileName });
 		}
 		console.log(`File with ID "${fileId}" was successfully download to "${destOutputPath}"`);
 	})
