6112bd847142022149d4ac2ae42dc8c138f904dd
Mati <matibat@gmail.com>
2021-12-15 13:06:44 -0300

    test(download folder): implement unit tests for getOutputFolderPathAndName PE-780

---
+++ src/utils/local_folder_path.test.ts
@@ -0,0 +1,75 @@
+import { expect } from 'chai';
+import { join as joinPath } from 'path';
+import { stub } from 'sinon';
+import { getOutputFolderPathAndName } from './local_folder_path';
+
+const NAME_EXSTING_FOLDER_NAME = 'EXISTING_folder';
+const NAME_EXISTING_FILE = 'EXISTING_file.txt';
+// const NAME_EXISTING_NON_FILE = 'EXISTING_socket_or_symbolic_link';
+// const NAME_NONEXISTENT_FILE = 'NONEXISTENT_crazy_file_with_cool_stuff.doc';
+const NAME_NONEXISTENT_FOLDER = 'NONEXISTENT_folder';
+
+const PATH_EXISTING_PARENT_FOLDER = '/my/existing';
+const PATH_EXISTING_FOLDER = joinPath(PATH_EXISTING_PARENT_FOLDER, NAME_EXSTING_FOLDER_NAME);
+const PATH_EXISTING_FILE = joinPath(PATH_EXISTING_PARENT_FOLDER, NAME_EXISTING_FILE);
+const PATH_UNEXISTENT_FOLDER = joinPath(PATH_EXISTING_PARENT_FOLDER, NAME_NONEXISTENT_FOLDER);
+// const PATH_EXISTING_NON_FILE = joinPath(PATH_EXISTING_PARENT_FOLDER, NAME_EXISTING_NON_FILE);
+
+// const PATH_NONEXISTENT_FILE = joinPath(PATH_EXISTING_FOLDER, NAME_NONEXISTENT_FILE);
+const PATH_NONEXISTENT_PATH = '/some/NONEXISTENT/path';
+
+const mockStatsFolder = {
+	isDirectory: () => true,
+	isFile: () => false
+};
+
+const mockStatsFile = {
+	isDirectory: () => false,
+	isFile: () => true
+};
+
+// const mockStatsNotFileNorFolder = {
+// 	isDirectory: () => false,
+// 	isFile: () => false
+// };
+
+describe('getOutputFolderPathAndName function', () => {
+	const fsStatSyncAndPathResolveWrapper = {
+		statSync: stub(),
+		resolve: stub()
+	};
+
+	before(() => {
+		// makes the resolve function to return the same given path
+		fsStatSyncAndPathResolveWrapper.resolve.returnsArg(0);
+
+		// makes the stubbed statsSync method to return a mocked fs.Stats for each corresponding case
+		fsStatSyncAndPathResolveWrapper.statSync.withArgs(PATH_EXISTING_PARENT_FOLDER).returns(mockStatsFolder);
+		fsStatSyncAndPathResolveWrapper.statSync.withArgs(PATH_EXISTING_FOLDER).returns(mockStatsFolder);
+		fsStatSyncAndPathResolveWrapper.statSync.withArgs(PATH_EXISTING_FILE).returns(mockStatsFile);
+		// fsStatSyncAndPathResolveWrapper.statSync.withArgs(PATH_EXISTING_NON_FILE).returns(mockStatsNotFileNorFolder);
+		// the stub throws for the nonexistent paths
+		fsStatSyncAndPathResolveWrapper.statSync.throws();
+	});
+
+	it('returns only the providen path if it is a directory', () => {
+		expect(getOutputFolderPathAndName(PATH_EXISTING_FOLDER, fsStatSyncAndPathResolveWrapper)).to.deep.equal([
+			PATH_EXISTING_FOLDER
+		]);
+	});
+
+	it('returns the parent path and the basename if the path in nonexistent but the parent is a directory', () => {
+		expect(getOutputFolderPathAndName(PATH_UNEXISTENT_FOLDER, fsStatSyncAndPathResolveWrapper)).to.deep.equal([
+			PATH_EXISTING_PARENT_FOLDER,
+			NAME_NONEXISTENT_FOLDER
+		]);
+	});
+
+	it("throws if the path isn't a directory", () => {
+		expect(() => getOutputFolderPathAndName(PATH_EXISTING_FILE, fsStatSyncAndPathResolveWrapper)).to.throw();
+	});
+
+	it('throws if the path nor its parent exist', () => {
+		expect(() => getOutputFolderPathAndName(PATH_NONEXISTENT_PATH, fsStatSyncAndPathResolveWrapper)).to.throw();
+	});
+});
--- src/utils/local_folder_path.ts
+++ src/utils/local_folder_path.ts
@@ -4,16 +4,28 @@ import { basename, dirname, resolve } from 'path';
 export type FilePathAndName = [string, string?]; // TODO: consolidate with the download-file functionality
 
 /**
+ * For testing purposes.
+ * This object is the default value of the last argument of getOutputFolderPathAndName
+ */
+const fsStatSyncAndPathResolve = {
+	statSync,
+	resolve
+};
+
+/**
  * Resolves the path, verifies its existance and type to conditionally return its dir and file name
  * @param {string} destOutputPath - the path from where to extract the dir path and name
  * @returns {FilePathAndName} - the directory where to put the file and the file name (which is undefined when the provided destOutputPath is a directory)
  */
-export function getOutputFolderPathAndName(destOutputPath: string): FilePathAndName {
-	const resolvedOutputPath = resolve(destOutputPath);
+export function getOutputFolderPathAndName(
+	destOutputPath: string,
+	fsStatSyncAndPathResolveWrapper = fsStatSyncAndPathResolve
+): FilePathAndName {
+	const resolvedOutputPath = fsStatSyncAndPathResolveWrapper.resolve(destOutputPath);
 	const outputDirname = dirname(resolvedOutputPath);
 	const outputBasename = basename(resolvedOutputPath);
 	try {
-		const outputPathStats = statSync(resolvedOutputPath);
+		const outputPathStats = fsStatSyncAndPathResolveWrapper.statSync(resolvedOutputPath);
 		// the destination does exist
 		if (outputPathStats.isDirectory()) {
 			// and is a directory
@@ -22,7 +34,7 @@ export function getOutputFolderPathAndName(destOutputPath: string): FilePathAndN
 		throw new Error(`The destination isn't a folder!`);
 	} catch (e) {
 		// the destination doesn't exist
-		const outputParentPathStats = statSync(outputDirname);
+		const outputParentPathStats = fsStatSyncAndPathResolveWrapper.statSync(outputDirname);
 		// the parent exists
 		if (outputParentPathStats.isDirectory()) {
 			return [outputDirname, outputBasename];
