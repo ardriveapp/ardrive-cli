1f5e78c50ef69bae411a3272095153ba596cc0fc
Derek Sonnenberg <derek@ardrive.io>
2021-12-22 15:36:10 -0600

    test(network price estimator): Add BATS test to compare network reward vs upload reward PE-821

---
+++ bats_test/upload-file/create-file.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+yes "10 chunk test"  | head --bytes 2621440 > /home/node/10Chunks.txt
+
+exit 0
---
+++ bats_test/upload-file/reward.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+curl -s https://arweave.net/price/2621440 | cut -c 1-
+exit 0
