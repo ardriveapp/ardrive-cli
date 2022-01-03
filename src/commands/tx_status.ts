f4837bf1d4ec018075340d1780abc41cdfa5fa73
Ariel Melendez <ariel@ardrive.io>
2022-01-03 10:54:02 -0800

    feat(tx status): Output tx status in JSON with bundle info

--- src/commands/tx_status.ts
+++ src/commands/tx_status.ts
@@ -1,4 +1,4 @@
-import { fetchMempool, TransactionID, TxID } from 'ardrive-core-js';
+import { fetchMempool, GQLEdgeInterface, TransactionID, TxID } from 'ardrive-core-js';
 import { cliArweave } from '..';
 import { CLICommand, ParametersHelper } from '../CLICommand';
 import { CLIAction } from '../CLICommand/action';
@@ -6,6 +6,7 @@ import { ERROR_EXIT_CODE, SUCCESS_EXIT_CODE } from '../CLICommand/error_codes';
 import { ConfirmationsParameter, TransactionIdParameter } from '../parameter_declarations';
 
 const MIN_CONFIRMATIONS = 15;
+const graphQLURL = 'https://arweave.net/graphql';
 
 new CLICommand({
 	name: 'tx-status',
@@ -14,29 +15,66 @@ new CLICommand({
 		const parameters = new ParametersHelper(options);
 		const confirmations = parameters.getParameterValue<number>(ConfirmationsParameter, Number) ?? MIN_CONFIRMATIONS;
 		const txId = parameters.getRequiredParameterValue(TransactionIdParameter, TxID);
+		const output: Record<string, string | number> = {
+			txId: `${txId}`
+		};
+
+		// Check the mempool to see if it's pending
 		const transactionsInMempool = (await fetchMempool()).map((id) => new TransactionID(id));
 		const pending = transactionsInMempool.some((tx) => tx.equals(txId));
 		if (pending) {
-			console.log(`${txId}: Pending`);
+			output['status'] = 'Pending';
+			console.log(output, null, 4);
 			return SUCCESS_EXIT_CODE;
 		}
 
-		const confStatus = (await cliArweave.transactions.getStatus(`${txId}`)).confirmed;
+		// Check for a bundled tx
+		const queryResponse = await cliArweave.api.post(graphQLURL, {
+			query: `
+				query {
+					transactions(ids: ["${txId}"]) {
+						edges {
+							node {
+								block{
+									height
+								}
+								bundledIn{
+									id
+								}
+								id
+							}
+						}
+					}
+				}`
+		});
 
-		if (!confStatus?.block_height) {
-			console.log(`${txId}: Not found`);
+		const edges: GQLEdgeInterface[] = queryResponse.data.data.transactions.edges;
+		if (!edges.length) {
+			output['status'] = 'Not Found';
+			console.log(JSON.stringify(output, null, 4));
 			return ERROR_EXIT_CODE;
 		}
 
-		if (confStatus?.number_of_confirmations >= confirmations) {
-			console.log(
-				`${txId}: Mined at block height ${confStatus.block_height} with ${confStatus.number_of_confirmations} confirmations`
-			);
-		} else {
-			console.log(
-				`${txId}: Confirming at block height ${confStatus.block_height} with ${confStatus.number_of_confirmations} confirmations`
-			);
+		const bundledInTx = (edges[0].node as Record<string, any>)['bundledIn']?.id as string;
+		if (bundledInTx) {
+			output['bundleTxId'] = bundledInTx;
 		}
+
+		output['status'] = 'Confirming';
+		const txHeight = edges[0].node.block.height;
+		if (txHeight) {
+			if (txHeight >= confirmations) {
+				output['status'] = 'Mined';
+				output['height'] = txHeight;
+			}
+
+			// Get the current block height so we can compute confirmations
+			const rootRsp = await cliArweave.api.get('https://arweave.net/');
+			const currHeight = rootRsp.data.height;
+			output['confirmations'] = currHeight - txHeight;
+		}
+
+		console.log(JSON.stringify(output, null, 4));
 
 		return SUCCESS_EXIT_CODE;
 	})
