b05c368c287d071925d360e2d4558a0ad6010d25
Matías Batista <matibat@gmail.com>
2022-01-11 13:49:57 -0300

    test(download drive): PE-870

--- bats_test/downloads/drive-downloads/cleanup.sh
+++ bats_test/downloads/drive-downloads/cleanup.sh
@@ -5,7 +5,6 @@ DIR="$(cd "$(dirname "${BATS_TEST_FILENAME}")" >/dev/null 2>&1 && pwd)"
 rm -rf \
     "${DIR}/Files With a Variety of Extensions Public" \
     "${DIR}/My custom folder name" \
-    "${DIR}/existing_file.txt" \
-    "${DIR}/existing_fifo"
+    "${DIR}/existing_file.txt"
 
 exit 0
