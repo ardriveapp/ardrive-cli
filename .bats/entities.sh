1546469fb08345770b633fcea6d0e9110e9c5e0b
Mati <matibat@gmail.com>
2022-01-24 15:48:19 -0300

    test(entities.sh): Adds another ENV variable for an existing fileName PE-883

--- .bats/entities.sh
+++ .bats/entities.sh
@@ -14,10 +14,11 @@ pubDriveList="$(echo $cache | jq -rc '.[] | select(.drivePrivacy == "public") | 
 for publicDrive in ${pubDriveList[@]}; do
     aux="$(yarn ardrive list-drive -d $publicDrive)"
     numberFiles="$(echo $aux | jq -r '[.[] | select(.entityType == "file")] | length')"
-    if [ $numberFiles != 0 ]; then
+    if [ "$numberFiles" -ge "2" ]; then
         fileID="$(echo $aux | jq -r '[.[] | select(.entityType == "file")][0] | .entityId')"
         fileSize="$(echo $aux | jq -r '[.[] | select(.entityType == "file")][0] | .size')"
         fileName="$(echo $aux | jq -r '[.[] | select(.entityType == "file")][0] | .name')"
+        fileNameAlt="$(echo $aux | jq -r '[.[] | select(.entityType == "file")][1] | .name')"
         folderID="$(echo $aux | jq -r '[.[] | select(.entityType == "file")][0] | .parentFolderId')"
         rootID="$(echo $aux | jq -r '[.[] | select(.parentFolderId == "root folder")][0] | .entityId')"
         driveID="$(echo $aux | jq -r '[.[] | select(.entityType == "file")][0] | .driveId')"
@@ -28,6 +29,7 @@ done
 export PUB_FILE_ID="$fileID"
 export PUB_FILE_SIZE="$fileSize" #in bytes
 export PUB_FILE_NAME="$fileName"
+export PUB_FILE_NAME_ALT="$fileNameAlt"
 export PARENT_FOLDER_ID="$folderID"
 export ROOT_FOLDER_ID="$rootID"
 export PUB_DRIVE_ID="$driveID"
