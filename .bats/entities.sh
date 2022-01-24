133f226983b11e1794c58d8a301ddf58ef014794
agsuy <agustin@ardrive.io>
2022-01-24 12:03:47 -0300

    feature(entities): Support public file attr

--- .bats/entities.sh
+++ .bats/entities.sh
@@ -1,10 +1,36 @@
 #!/bin/bash
 
+#This script parses a provided $WALLET, and sets ENV variables using the values from the first valid Public file.
+
+## TODO: make this conditional with a flag to avoid reset on new SHELL session
 cd $HOME/ardrive-cli/
 
 cache="$(yarn ardrive list-all-drives -w $WALLET)"
-driveID="$(echo $cache | jq '[.. | select(.drivePrivacy? == "public")][0] | .driveId' | cut -d '"' -f 2)"
-folderID="$(echo $cache | jq '[.. | select(.drivePrivacy? == "public")][0] | .rootFolderId' | cut -d '"' -f 2)"
 
+# RAW, one line, no brackets and no quotes
+pubDriveList="$(echo $cache | jq -rc '.[] | select(.drivePrivacy == "public") | .driveId')"
+
+#Iterates over list of driveIDs from wallet.
+for publicDrive in ${pubDriveList[@]}; do
+    aux="$(yarn ardrive list-drive -d $publicDrive)"
+    numberFiles="$(echo $aux | jq -r '[.[] | select(.entityType == "file")] | length')"
+    if [ $numberFiles != 0 ]; then
+        fileID="$(echo $aux | jq -r '[.[] | select(.entityType == "file")][0] | .entityId')"
+        fileSize="$(echo $aux | jq -r '[.[] | select(.entityType == "file")][0] | .size')"
+        fileName="$(echo $aux | jq -r '[.[] | select(.entityType == "file")][0] | .name')"
+        folderID="$(echo $aux | jq -r '[.[] | select(.entityType == "file")][0] | .parentFolderId')"
+        rootID="$(echo $aux | jq -r '[.[] | select(.parentFolderId == "root folder")][0] | .entityId')"
+        driveID="$(echo $aux | jq -r '[.[] | select(.entityType == "file")][0] | .driveId')"
+        break
+    fi
+done
+
+export PUB_FILE_ID="$fileID"
+export PUB_FILE_SIZE="$fileSize" #in bytes
+export PUB_FILE_NAME="$fileName"
+export PARENT_FOLDER_ID="$folderID"
+export ROOT_FOLDER_ID="$rootID"
 export PUB_DRIVE_ID="$driveID"
-export PUB_FOLD_ID="$folderID"
+
+# To remain compatible with previous behavior
+export PUB_FOLD_ID="$rootID"
