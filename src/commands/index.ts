9b01660545d52dd6df753b9d3f38809ae7f33112
Mati <matibat@gmail.com>
2022-01-25 11:49:44 -0300

    feat(rename folder): Implements the rename-folder commandf PE-897

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -24,6 +24,7 @@ import './list_folder';
 import './move_file';
 import './move_folder';
 import './rename_file';
+import './rename_folder';
 import './send_ar';
 import './send_tx';
 import './tx_status';
