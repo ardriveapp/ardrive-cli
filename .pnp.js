ea14f4f180d05df5966440bb87ad211756d35de5
Mati <matibat@gmail.com>
2022-01-27 14:30:50 -0300

    feat(rename folder): bump core dep PE-897

--- .pnp.js
+++ .pnp.js
@@ -48,7 +48,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@types/source-map-support", "npm:0.5.4"],
             ["@typescript-eslint/eslint-plugin", "virtual:6f50bb9424c73c7612c66dab5cf8914d8ec79550c84d8ca5e4888e80022682c708b4b5a1c510d282a03285cc9bb19002b477ae70d15882aa995ea1d5d6bf24ab#npm:4.20.0"],
             ["@typescript-eslint/parser", "virtual:6f50bb9424c73c7612c66dab5cf8914d8ec79550c84d8ca5e4888e80022682c708b4b5a1c510d282a03285cc9bb19002b477ae70d15882aa995ea1d5d6bf24ab#npm:4.20.0"],
-            ["ardrive-core-js", "npm:1.7.0-alpha-1"],
+            ["ardrive-core-js", "npm:1.7.0-alpha-2"],
             ["arweave", "npm:1.10.18"],
             ["axios", "npm:0.21.1"],
             ["chai", "npm:4.3.4"],
@@ -1869,7 +1869,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@types/source-map-support", "npm:0.5.4"],
             ["@typescript-eslint/eslint-plugin", "virtual:6f50bb9424c73c7612c66dab5cf8914d8ec79550c84d8ca5e4888e80022682c708b4b5a1c510d282a03285cc9bb19002b477ae70d15882aa995ea1d5d6bf24ab#npm:4.20.0"],
             ["@typescript-eslint/parser", "virtual:6f50bb9424c73c7612c66dab5cf8914d8ec79550c84d8ca5e4888e80022682c708b4b5a1c510d282a03285cc9bb19002b477ae70d15882aa995ea1d5d6bf24ab#npm:4.20.0"],
-            ["ardrive-core-js", "npm:1.7.0-alpha-1"],
+            ["ardrive-core-js", "npm:1.7.0-alpha-2"],
             ["arweave", "npm:1.10.18"],
             ["axios", "npm:0.21.1"],
             ["chai", "npm:4.3.4"],
@@ -1895,10 +1895,10 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
         }]
       ]],
       ["ardrive-core-js", [
-        ["npm:1.7.0-alpha-1", {
-          "packageLocation": "./.yarn/cache/ardrive-core-js-npm-1.7.0-alpha-1-474c660e02-b26b92fd5d.zip/node_modules/ardrive-core-js/",
+        ["npm:1.7.0-alpha-2", {
+          "packageLocation": "./.yarn/cache/ardrive-core-js-npm-1.7.0-alpha-2-cfc7746e6f-c01c408b51.zip/node_modules/ardrive-core-js/",
           "packageDependencies": [
-            ["ardrive-core-js", "npm:1.7.0-alpha-1"],
+            ["ardrive-core-js", "npm:1.7.0-alpha-2"],
             ["@alexsasharegan/simple-cache", "npm:3.3.3"],
             ["arbundles", "npm:0.5.5"],
             ["arweave", "npm:1.10.18"],
