131154221bed8c07d66907dc4e07916dbe1f5e7f
Derek Sonnenberg <derek@ardrive.io>
2022-02-04 13:30:49 -0600

    feat(custom gateway): Add custom gateway to base-reward command for prototyping PE-480

--- src/commands/base_reward.ts
+++ src/commands/base_reward.ts
@@ -2,20 +2,21 @@ import { ByteCount } from 'ardrive-core-js';
 import { CLICommand, ParametersHelper } from '../CLICommand';
 import { CLIAction } from '../CLICommand/action';
 import { SUCCESS_EXIT_CODE } from '../CLICommand/error_codes';
-import { BoostParameter } from '../parameter_declarations';
+import { BoostParameter, GatewayParameter } from '../parameter_declarations';
 import axios, { AxiosResponse } from 'axios';
 
-async function getBaseReward(byteCount?: ByteCount): Promise<string> {
-	const response: AxiosResponse = await axios.get(`https://arweave.net/price/${byteCount ?? 0}`);
+async function getBaseReward(gateway: URL, byteCount?: ByteCount): Promise<string> {
+	const response: AxiosResponse = await axios.get(`${gateway.href}price/${byteCount ?? 0}`);
 	return `${response.data}`;
 }
 
 new CLICommand({
 	name: 'base-reward',
-	parameters: [BoostParameter],
+	parameters: [BoostParameter, GatewayParameter],
 	action: new CLIAction(async function action(options) {
 		const parameters = new ParametersHelper(options);
-		let baseRewardStr = await getBaseReward();
+		const gateway = parameters.getGateway();
+		let baseRewardStr = await getBaseReward(gateway);
 		const multiple = parameters.getOptionalBoostSetting();
 		if (multiple) {
 			baseRewardStr = multiple.boostReward(baseRewardStr);
