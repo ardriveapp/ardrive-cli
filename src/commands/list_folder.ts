ecb2c17444d4a7de503220c870b81caeb20d4c06
Mati <matibat@gmail.com>
2022-02-04 09:27:55 -0300

    feat(entities with keys): makes the list-folder cmd to pass a boolean withKeys to ArDrive to suport the new functionality PE-973

--- src/commands/list_folder.ts
+++ src/commands/list_folder.ts
@@ -8,14 +8,19 @@ import { cliArDriveAnonymousFactory, cliArDriveFactory } from '..';
 import { CLICommand, ParametersHelper } from '../CLICommand';
 import { CLIAction } from '../CLICommand/action';
 import { SUCCESS_EXIT_CODE } from '../CLICommand/error_codes';
-import { DrivePrivacyParameters, ParentFolderIdParameter, TreeDepthParams } from '../parameter_declarations';
+import {
+	DrivePrivacyParameters,
+	ParentFolderIdParameter,
+	TreeDepthParams,
+	WithKeysParameter
+} from '../parameter_declarations';
 
 new CLICommand({
 	name: 'list-folder',
-	parameters: [ParentFolderIdParameter, ...TreeDepthParams, ...DrivePrivacyParameters],
+	parameters: [ParentFolderIdParameter, WithKeysParameter, ...TreeDepthParams, ...DrivePrivacyParameters],
 	action: new CLIAction(async function action(options) {
 		const parameters = new ParametersHelper(options);
-		const folderId = EID(parameters.getRequiredParameterValue(ParentFolderIdParameter));
+		const folderId = parameters.getRequiredParameterValue(ParentFolderIdParameter, EID);
 		let children: (ArFSPrivateFileOrFolderWithPaths | ArFSPublicFileOrFolderWithPaths)[];
 		const maxDepth = await parameters.getMaxDepth(0);
 
@@ -25,11 +30,12 @@ new CLICommand({
 
 			const driveId = await arDrive.getDriveIdForFolderId(folderId);
 			const driveKey = await parameters.getDriveKey({ driveId });
+			const withKeys = await parameters.getParameterValue(WithKeysParameter, (value) => !!value);
 
 			// We have the drive id from deriving a key, we can derive the owner
 			const driveOwner = await arDrive.getOwnerForDriveId(driveId);
 
-			children = await arDrive.listPrivateFolder({ folderId, driveKey, maxDepth, owner: driveOwner });
+			children = await arDrive.listPrivateFolder({ folderId, driveKey, maxDepth, owner: driveOwner, withKeys });
 		} else {
 			const arDrive = cliArDriveAnonymousFactory({});
 			children = await arDrive.listPublicFolder({ folderId, maxDepth });
