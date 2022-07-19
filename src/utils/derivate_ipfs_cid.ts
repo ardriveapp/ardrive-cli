4ba89296c59c1a07e95d19c288410349025ce366
Mati <matibat@gmail.com>
2022-07-19 16:44:09 -0300

    feat(derivate ipfs cid): implements the derivateIpfsCid function PE-1885

---
+++ src/utils/derivate_ipfs_cid.ts
@@ -0,0 +1,9 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const CID = require('ipfs-only-hash');
+
+type DataInput = string | Uint8Array | AsyncIterable<Uint8Array>;
+
+export async function derivateIpfsCid(dataInput: DataInput): Promise<string> {
+	const hash = await CID.of(dataInput);
+	return hash;
+}
