8d348aa8aeb59d755388ae72a0415036253cfcce
Mati <matibat@gmail.com>
2022-07-21 12:32:40 -0300

    test(derive IPFS CID): renames variable names PE-1885

--- src/utils/derive_ipfs_cid.test.ts
+++ src/utils/derive_ipfs_cid.test.ts
@@ -2,16 +2,16 @@ import { expect } from 'chai';
 import { deriveIpfsCid } from './ipfs_utils';
 
 const dataToHash = [
-	{ data: 'hello', hash: 'QmWfVY9y3xjsixTgbd9AorQxH7VtMpzfx2HaWtsoUYecaX' },
-	{ data: 'my file data', hash: 'Qmd8s28wFUPP2rRbQEvvoHh84G22ShP5YzNgvT2mdhyrmq' },
-	{ data: Buffer.from('my file data'), hash: 'Qmd8s28wFUPP2rRbQEvvoHh84G22ShP5YzNgvT2mdhyrmq' }
+	{ inputData: 'hello', expectedHash: 'QmWfVY9y3xjsixTgbd9AorQxH7VtMpzfx2HaWtsoUYecaX' },
+	{ inputData: 'my file data', expectedHash: 'Qmd8s28wFUPP2rRbQEvvoHh84G22ShP5YzNgvT2mdhyrmq' },
+	{ inputData: Buffer.from('my file data'), expectedHash: 'Qmd8s28wFUPP2rRbQEvvoHh84G22ShP5YzNgvT2mdhyrmq' }
 ];
 
 describe('deriveIpfsCid function', () => {
 	it('returns the expeced hash', async () => {
-		for (const expected of dataToHash) {
-			const actualCid = await deriveIpfsCid(expected.data);
-			expect(actualCid).to.equal(expected.hash);
+		for (const testCaseData of dataToHash) {
+			const actualCid = await deriveIpfsCid(testCaseData.inputData);
+			expect(actualCid).to.equal(testCaseData.expectedHash);
 		}
 	});
 });
