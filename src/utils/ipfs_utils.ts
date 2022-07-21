f5f33ce4fdf7052d19cdf66f14fa55e45a7ed29b
Mati <matibat@gmail.com>
2022-07-21 12:28:49 -0300

    chore(derive IPFS CID): renames the input argument; adds a JSDoc block PE-1885

--- src/utils/ipfs_utils.ts
+++ src/utils/ipfs_utils.ts
@@ -1,9 +1,13 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const CID = require('ipfs-only-hash');
 
-type DataInput = string | Uint8Array | AsyncIterable<Uint8Array>;
+type CIDHashable = string | Uint8Array | AsyncIterable<Uint8Array>;
 
-export async function deriveIpfsCid(dataInput: DataInput): Promise<string> {
+/**
+ * Generates a v1 IPFS CID hash for the given data
+ * @param {CIDHashable} dataInput is the raw data of the file
+ */
+export async function deriveIpfsCid(dataInput: CIDHashable): Promise<string> {
 	const hash = await CID.of(dataInput);
 	return hash;
 }
