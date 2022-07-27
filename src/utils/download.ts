32f4b458fad81dc461221d417fc9082e5542d21e
jdaev <jdhn42@gmail.com>
2022-07-27 10:29:36 -0400

    refactor: follow remote path redirects PE-1904

--- src/utils/download.ts
+++ src/utils/download.ts
@@ -8,18 +8,25 @@ import * as fs from 'fs';
  */
 export async function download(url: string, filePath: string): Promise<string | undefined> {
 	const proto = !url.charAt(4).localeCompare('s') ? https : http;
-
+	console.log(filePath);
 	return new Promise((resolve, reject) => {
-		const file = fs.createWriteStream(filePath);
+		const file = fs.createWriteStream(filePath, { flags: 'wx' });
 
 		const request = proto.get(url, (response) => {
-			if (response.statusCode !== 200) {
+			if (response.statusCode === 302 || response.statusCode === 301) {
+				//Recursively follow redirects, only a 200 will resolve.
+				if (response.headers.location) {
+					download(response.headers.location, filePath).then((filePath: string | undefined) =>
+						resolve(filePath)
+					);
+				}
+			} else if (response.statusCode !== 200) {
 				fs.unlink(filePath, () => {
 					reject(new Error(`Failed to get '${url}' (${response.statusCode})`));
 				});
 				return;
 			}
-
+			console.log(file.path);
 			response.pipe(file);
 		});
 
