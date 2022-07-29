e6bea82ddf4daf4584a73c1de5de6e55fc237cc7
Derek Sonnenberg <derek@ardrive.io>
2022-07-29 12:18:05 -0500

    chore(core version): Bump alpha core for QA testing PE-1941

--- .pnp.js
+++ .pnp.js
@@ -50,7 +50,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@types/source-map-support", "npm:0.5.4"],
             ["@typescript-eslint/eslint-plugin", "virtual:6f50bb9424c73c7612c66dab5cf8914d8ec79550c84d8ca5e4888e80022682c708b4b5a1c510d282a03285cc9bb19002b477ae70d15882aa995ea1d5d6bf24ab#npm:4.20.0"],
             ["@typescript-eslint/parser", "virtual:6f50bb9424c73c7612c66dab5cf8914d8ec79550c84d8ca5e4888e80022682c708b4b5a1c510d282a03285cc9bb19002b477ae70d15882aa995ea1d5d6bf24ab#npm:4.20.0"],
-            ["ardrive-core-js", "npm:1.16.0"],
+            ["ardrive-core-js", "npm:1.17.0-alpha-0"],
             ["arweave", "npm:1.11.4"],
             ["axios", "npm:0.21.1"],
             ["bn.js", "npm:5.2.1"],
@@ -2026,7 +2026,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@types/source-map-support", "npm:0.5.4"],
             ["@typescript-eslint/eslint-plugin", "virtual:6f50bb9424c73c7612c66dab5cf8914d8ec79550c84d8ca5e4888e80022682c708b4b5a1c510d282a03285cc9bb19002b477ae70d15882aa995ea1d5d6bf24ab#npm:4.20.0"],
             ["@typescript-eslint/parser", "virtual:6f50bb9424c73c7612c66dab5cf8914d8ec79550c84d8ca5e4888e80022682c708b4b5a1c510d282a03285cc9bb19002b477ae70d15882aa995ea1d5d6bf24ab#npm:4.20.0"],
-            ["ardrive-core-js", "npm:1.16.0"],
+            ["ardrive-core-js", "npm:1.17.0-alpha-0"],
             ["arweave", "npm:1.11.4"],
             ["axios", "npm:0.21.1"],
             ["bn.js", "npm:5.2.1"],
@@ -2053,10 +2053,10 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
         }]
       ]],
       ["ardrive-core-js", [
-        ["npm:1.16.0", {
-          "packageLocation": "./.yarn/cache/ardrive-core-js-npm-1.16.0-69f22a8065-374a87eafe.zip/node_modules/ardrive-core-js/",
+        ["npm:1.17.0-alpha-0", {
+          "packageLocation": "./.yarn/cache/ardrive-core-js-npm-1.17.0-alpha-0-12a9142736-c0ce007f6d.zip/node_modules/ardrive-core-js/",
           "packageDependencies": [
-            ["ardrive-core-js", "npm:1.16.0"],
+            ["ardrive-core-js", "npm:1.17.0-alpha-0"],
             ["@alexsasharegan/simple-cache", "npm:3.3.3"],
             ["arbundles", "npm:0.6.19"],
             ["arweave", "npm:1.11.4"],
