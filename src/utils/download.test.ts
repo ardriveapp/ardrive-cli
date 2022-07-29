cbd3819e248856ad83568a390dc3fc5e90eae0b4
jdaev <jdhn42@gmail.com>
2022-07-29 12:02:00 -0400

    test: add invalid download test PE-1904

--- src/utils/download.test.ts
+++ src/utils/download.test.ts
@@ -5,9 +5,20 @@ import * as fs from 'fs';
 
 describe('download test', () => {
 	const validDownloadLink = 'https://arweave.net/pVoSqZgJUCiNw7oS6CtlVEd8gREQlpRbccrsMLkeIuQ';
+	const invalidDownloadLink = 'https://arweave.net/pVoSqZgJUCiNw7oS6CtlVEV8gREQlpRbccrsMLkeIuQ';
 	const tempFolderPath = getTempFolder();
 	it('downloads a file into the provided folder when given a valid link', async () => {
 		const result = await download(validDownloadLink, tempFolderPath);
 		expect(fs.existsSync(result)).to.equal(true);
+	});
+
+	it('download throws when given an invalid link', async () => {
+		let error;
+		try {
+			await download(invalidDownloadLink, tempFolderPath);
+		} catch (err) {
+			error = err;
+		}
+		expect(error?.name).to.equal('Error');
 	});
 });
