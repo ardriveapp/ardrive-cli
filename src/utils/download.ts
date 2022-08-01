2d0e2af2fa9ea39556251906a7618b63e3e6a7de
jdaev <jdhn42@gmail.com>
2022-08-01 16:44:11 -0400

    refactor: download file as stream PE-1904

--- src/utils/download.ts
+++ src/utils/download.ts
@@ -1,24 +1,46 @@
 import * as fs from 'fs';
 import path from 'path';
-import axios, { AxiosResponse } from 'axios';
+import axios from 'axios';
+import { uniqueId } from 'lodash';
 
 /**
  * Downloads file from remote HTTP[S] host and puts its contents to the
  * specified location.
  * @param url URL of the file to download.
  * @param destinationPath Path to the destination file.
+ * @param fileName The file name. If no name is provided a random name will be used.
  */
-export async function download(url: string, filePath: string): Promise<string> {
-	const fileName = url.split('/').pop();
-	if (!fileName) {
-		throw new Error('Invalid remote path. No file name found.');
-	}
+export async function download(url: string, destinationPath: string, fileName?: string): Promise<string> {
+	const name = fileName ?? url.split('/').pop() ?? uniqueId();
+	const pathToFile = path.join(destinationPath, name);
 	try {
-		const response: AxiosResponse = await axios.get(url, { responseType: 'arraybuffer' });
-		const pathToFile = path.join(filePath, fileName);
+		const writer = fs.createWriteStream(pathToFile);
 
-		fs.writeFileSync(pathToFile, response.data);
-		return pathToFile;
+		return axios({
+			method: 'get',
+			url: url,
+			responseType: 'stream'
+		}).then((response) => {
+			//ensure that the user can call `then()` only when the file has
+			//been downloaded entirely.
+
+			return new Promise((resolve, reject) => {
+				response.data.pipe(writer);
+				let error: Error | null = null;
+				writer.on('error', (err) => {
+					error = err;
+					writer.close();
+					reject(err);
+				});
+				writer.on('close', () => {
+					if (!error) {
+						resolve(pathToFile);
+					}
+					//no need to call the reject here, as it will have been called in the
+					//'error' stream;
+				});
+			});
+		});
 	} catch (error) {
 		throw new Error(`Failed to download file from remote path ${url}: ${error.message}`);
 	}
