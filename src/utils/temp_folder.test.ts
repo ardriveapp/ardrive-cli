e710f2a58fd027517c3862ab068ac80878a9d23e
Javed Hussein <jdhn42@gmail.com>
2022-08-03 10:28:30 -0400

    test: add more describes PE-1904

--- src/utils/temp_folder.test.ts
+++ src/utils/temp_folder.test.ts
@@ -3,37 +3,41 @@ import { cleanUpTempFolder, getTempFolder } from './temp_folder';
 import * as fs from 'fs';
 import * as os from 'os';
 
-describe('getTempFolder function', () => {
-	it('returns a folder that exists', () => {
-		const tempFolderPath = getTempFolder();
-		expect(fs.existsSync(tempFolderPath)).to.equal(true);
+describe('temp folder functions', () => {
+	describe('getTempFolder function', () => {
+		it('returns a folder that exists', () => {
+			const tempFolderPath = getTempFolder();
+			expect(fs.existsSync(tempFolderPath)).to.equal(true);
+		});
+
+		it('getTempFolder can be called twice in a row', () => {
+			const tempFolderPath = getTempFolder();
+			const tempFolderPath2 = getTempFolder();
+			expect(fs.existsSync(tempFolderPath)).to.equal(true);
+			expect(fs.existsSync(tempFolderPath2)).to.equal(true);
+		});
+
+		it('returns a folder that contains the correct subfolders', () => {
+			const tempFolderPath = getTempFolder();
+			const expectedPathComponent = os.platform() === 'win32' ? '\\ardrive-temp' : '/.ardrive/temp';
+			expect(tempFolderPath).to.contains(expectedPathComponent);
+		});
 	});
 
-	it('getTempFolder can be called twice in a row', () => {
-		const tempFolderPath = getTempFolder();
-		const tempFolderPath2 = getTempFolder();
-		expect(fs.existsSync(tempFolderPath)).to.equal(true);
-		expect(fs.existsSync(tempFolderPath2)).to.equal(true);
-	});
-
-	it('returns a folder that contains the correct subfolders', () => {
-		const tempFolderPath = getTempFolder();
-		const expectedPathComponent = os.platform() === 'win32' ? '\\ardrive-temp' : '/.ardrive/temp';
-		expect(tempFolderPath).to.contains(expectedPathComponent);
-	});
-
-	it('cleanUpTempFolder removes the temporary folder from the local system', () => {
-		const tempFolderPath = getTempFolder();
-		expect(fs.existsSync(tempFolderPath)).to.equal(true);
-		cleanUpTempFolder();
-		expect(fs.existsSync(tempFolderPath)).to.equal(false);
-	});
-	it('cleanUpTempFolder can be called twice in a row', () => {
-		const tempFolderPath = getTempFolder();
-		expect(fs.existsSync(tempFolderPath)).to.equal(true);
-		cleanUpTempFolder();
-		expect(fs.existsSync(tempFolderPath)).to.equal(false);
-		cleanUpTempFolder();
-		expect(fs.existsSync(tempFolderPath)).to.equal(false);
+	describe('cleanUpTempFolder function', () => {
+		it('cleanUpTempFolder removes the temporary folder from the local system', () => {
+			const tempFolderPath = getTempFolder();
+			expect(fs.existsSync(tempFolderPath)).to.equal(true);
+			cleanUpTempFolder();
+			expect(fs.existsSync(tempFolderPath)).to.equal(false);
+		});
+		it('cleanUpTempFolder can be called twice in a row', () => {
+			const tempFolderPath = getTempFolder();
+			expect(fs.existsSync(tempFolderPath)).to.equal(true);
+			cleanUpTempFolder();
+			expect(fs.existsSync(tempFolderPath)).to.equal(false);
+			cleanUpTempFolder();
+			expect(fs.existsSync(tempFolderPath)).to.equal(false);
+		});
 	});
 });
