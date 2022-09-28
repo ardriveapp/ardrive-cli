724d72137744cf49c33d311493dfa5677bbd46e5
Mati <matibat@gmail.com>
2022-09-28 18:43:46 -0300

    feat(create-snapshot cmd): make it to throw Unimplemented Error PE-2243

--- src/commands/create_snapshot.ts
+++ src/commands/create_snapshot.ts
@@ -1,49 +1,44 @@
 import { CLICommand, ParametersHelper } from '../CLICommand';
 import {
 	BoostParameter,
-	FolderNameParameter,
+	// FolderNameParameter,
 	DryRunParameter,
-	ParentFolderIdParameter,
+	// ParentFolderIdParameter,
 	DrivePrivacyParameters,
 	GatewayParameter,
 	DriveIdParameter
 } from '../parameter_declarations';
-import { cliArDriveFactory } from '..';
+// import { cliArDriveFactory } from '..';
 import { SUCCESS_EXIT_CODE } from '../CLICommand/error_codes';
 import { CLIAction } from '../CLICommand/action';
-import { Wallet, EID } from 'ardrive-core-js';
-import { getArweaveFromURL } from '../utils/get_arweave_for_url';
+// import { Wallet, EID } from 'ardrive-core-js';
+// import { getArweaveFromURL } from '../utils/get_arweave_for_url';
 
 new CLICommand({
 	name: 'create-snapshot',
 	parameters: [DriveIdParameter, BoostParameter, DryRunParameter, ...DrivePrivacyParameters, GatewayParameter],
 	action: new CLIAction(async function action(options) {
 		const parameters = new ParametersHelper(options);
-		const wallet: Wallet = await parameters.getRequiredWallet();
-		const dryRun = parameters.isDryRun();
-		const arweave = getArweaveFromURL(parameters.getGateway());
+		// const wallet: Wallet = await parameters.getRequiredWallet();
+		// const dryRun = parameters.isDryRun();
+		// const arweave = getArweaveFromURL(parameters.getGateway());
 
-		const ardrive = cliArDriveFactory({
-			wallet,
-			feeMultiple: parameters.getOptionalBoostSetting(),
-			dryRun,
-			arweave
-		});
+		// const ardrive = cliArDriveFactory({
+		// 	wallet,
+		// 	feeMultiple: parameters.getOptionalBoostSetting(),
+		// 	dryRun,
+		// 	arweave
+		// });
 
-		const driveId = parameters.getRequiredParameterValue(DriveIdParameter, EID);
-		const folderName = parameters.getRequiredParameterValue(FolderNameParameter);
+		// const driveId = parameters.getRequiredParameterValue(DriveIdParameter, EID);
+		// const folderName = parameters.getRequiredParameterValue(FolderNameParameter);
 
 		const createFolderResult = await (async function () {
 			if (await parameters.getIsPrivate()) {
-				const driveKey = await parameters.getDriveKey({ driveId });
-				return ardrive.createPrivateFolder({
-					folderName,
-					driveId,
-					driveKey,
-					parentFolderId
-				});
+				// const driveKey = await parameters.getDriveKey({ driveId });
+				throw new Error('Unimplemented');
 			} else {
-				return ardrive.createPublicFolder({ folderName, driveId, parentFolderId });
+				throw new Error('Unimplemented');
 			}
 		})();
 		console.log(JSON.stringify(createFolderResult, null, 4));
