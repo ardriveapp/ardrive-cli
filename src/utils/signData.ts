8a9cec060e80e3f546dba0022bff8bb90c00077f
Derek Sonnenberg <derek@ardrive.io>
2023-04-27 16:00:44 -0500

    feat(turbo top up): add turbo balance route PE-3487

--- src/utils/signData.ts
+++ src/utils/signData.ts
@@ -1,10 +1,40 @@
-import crypto from 'crypto';
+import { JWKInterface } from 'arweave/node/lib/wallet';
+import crypto, { createPrivateKey, createPublicKey, KeyLike, KeyObject } from 'crypto';
+import { toB64Url } from './base64';
 
-export async function signData(privateKey: string, dataToSign: string): Promise<Uint8Array> {
+export async function signData(privateKey: KeyLike, dataToSign: string): Promise<Uint8Array> {
 	const sign = crypto.createSign('SHA256');
 	sign.update(dataToSign);
 
 	const signatureBuffer = sign.sign(privateKey);
 
 	return new Uint8Array(signatureBuffer);
+}
+
+export function jwkInterfaceToPublicKey(jwk: JWKInterface): KeyObject {
+	const publicKey = createPublicKey({
+		key: {
+			...jwk,
+			kty: 'RSA'
+		},
+		format: 'jwk'
+	});
+
+	return publicKey;
+}
+
+export function jwkInterfaceToPrivateKey(jwk: JWKInterface): KeyObject {
+	const privateKey = createPrivateKey({
+		key: {
+			...jwk,
+			kty: 'RSA'
+		},
+		format: 'jwk'
+	});
+
+	return privateKey;
+}
+
+export function publicKeyToHeader(publicKey: KeyObject) {
+	return toB64Url(Buffer.from(JSON.stringify(publicKey.export({ format: 'jwk' }))));
 }
