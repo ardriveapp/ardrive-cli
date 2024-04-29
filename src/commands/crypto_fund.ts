ad1de1e32e933fb6e08ea630d6e32b58e61cafa0
Derek Sonnenberg <derek@ardrive.io>
2024-04-29 16:21:06 -0500

    feat(solana payments): MVP for SOL topup on CLI PE-5993

--- src/commands/crypto_fund.ts
+++ src/commands/crypto_fund.ts
@@ -1,4 +1,4 @@
-import { AR, JWKWallet, Winston } from 'ardrive-core-js';
+import { AR, JWKWallet } from 'ardrive-core-js';
 import { CLICommand } from '../CLICommand';
 import { ParametersHelper } from '../CLICommand';
 import { CLIAction } from '../CLICommand/action';
@@ -10,7 +10,8 @@ import {
 	GatewayParameter,
 	WalletTypeParameters
 } from '../parameter_declarations';
-import { TurboFactory, WinstonToTokenAmount } from '@ardrive/turbo-sdk';
+import { TurboFactory, HexSolanaSigner } from '@ardrive/turbo-sdk';
+import bs58 from 'bs58';
 
 new CLICommand({
 	name: 'crypto-fund',
@@ -18,12 +19,17 @@ new CLICommand({
 	action: new CLIAction(async function action(options) {
 		const parameters = new ParametersHelper(options);
 		const arAmount = parameters.getRequiredParameterValue(ArAmountParameter, AR.from);
-		const winston: Winston = arAmount.toWinston();
+		const ar = arAmount;
 		const jwkWallet = (await parameters.getRequiredWallet()) as JWKWallet;
 
-		const turbo = TurboFactory.authenticated({ privateKey: jwkWallet.getPrivateKey() });
+		const signer = new HexSolanaSigner(bs58.encode(jwkWallet['jwk']));
+		const turbo = TurboFactory.authenticated({
+			signer,
+			gatewayUrl: 'https://api.devnet.solana.com',
+			paymentServiceConfig: { url: 'https://payment.ardrive.dev' }
+		});
 
-		const res = await turbo.topUpWithTokens({ tokenAmount: WinstonToTokenAmount(winston.valueOf()) });
+		const res = await turbo.topUpWithTokens({ tokenAmount: +ar.valueOf() * 1e9 });
 		console.log('res', JSON.stringify(res, null, 2));
 
 		return SUCCESS_EXIT_CODE;
