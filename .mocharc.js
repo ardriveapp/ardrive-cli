87221728f8e2757fc7367e9d4443c7034669967f
Derek Sonnenberg <fedellen@pixelpajamastudios.com>
2025-06-24 13:29:28 -0500

    test: dont run tests in node_modules PE-4841

--- .mocharc.js
+++ .mocharc.js
@@ -6,7 +6,7 @@ process.env.NODE_ENV = 'test';
 // Reference for options: https://github.com/mochajs/mocha/blob/master/example/config/.mocharc.js
 module.exports = {
 	extension: ['ts'],
-	spec: ['**/*.test.ts'],
+	spec: ['src/**/*.test.ts', 'tests/**/*.test.ts'],
 	require: ['ts-node/register/transpile-only', 'source-map-support/register', 'tests/testSetup.ts'],
 	timeout: '3000',
 	parallel: true,
